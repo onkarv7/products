dba6386f0401875b05d8a2237139f70457dd6d52
onkar vasav <onkarvasav@onkars-MacBook-Air.local>
2025-01-09 01:10:56 +0530

    SEO added

--- app/components/ProductCard.jsx
+++ app/components/ProductCard.jsx
@@ -19,17 +19,15 @@ const ProductCard = ({ product }) => {
         {" "}
         {product.title}{" "}
       </h3>{" "}
-
       <div className="price-container">
-      <p itemProp="price" className="product-price">
-        {" "}
-        {/* ${product.price}{" "} */}
-        Sign in or Create an account to see pricing
-      </p>{" "}
-       <Image alt="heart" src={heart} width={20} height={20} />
+        <p itemProp="price" className="product-price">
+          {" "}
+          {/* ${product.price}{" "} */}
+          Sign in or Create an account to see pricing
+        </p>{" "}
+        <Image alt="heart" src={heart} width={20} height={20} />
       </div>
-    
-\    </article>
+    </article>
   );
 };
 export default ProductCard;
