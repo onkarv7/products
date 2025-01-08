ac409c5979b71daf2b1423e5a71c7c8ade5e825d
onkar vasav <onkarvasav@onkars-MacBook-Air.local>
2025-01-08 23:12:04 +0530

    completed filters sidebar

--- app/components/FilterSidebar.jsx
+++ app/components/FilterSidebar.jsx
@@ -2,33 +2,63 @@ import React from "react";
 
 const FilterSidebar = () => {
   return (
-    <div>
-      <h3>Filters</h3>
-      <div className="filter-item">
-        <label>
-          <input type="checkbox" />
-          Customizable
-        </label>
-      </div>
-      <ul>
-        {[
-          "Ideal For",
-          "Occasion",
-          "Work",
-          "Fabric",
-          "Segment",
-          "Suitable For",
-          "Raw Materials",
-          "Pattern",
-        ].map((filter, index) => (
-          <li key={index} className="filter-item">
-            <button className="filter-button">
-              {filter}
-              <span>All</span>
-            </button>
+    <div classname="filter-sidebar">
+      <label classname="checkbox-container">
+        <input type="checkbox" />
+        CUSTOMIZABLE
+      </label>
+      <div classname="filter-section">
+        <h4>IDEAL FOR</h4>
+        <span>All</span>
+        <div>
+          <a href="#" classname="unselect-link">
+            Unselect all
+          </a>
+        </div>
+
+        <ul>
+          <li>
+            <input type="checkbox" id="men" />
+            <label htmlFor="men">Men</label>
           </li>
-        ))}
-      </ul>
+          <li>
+            <input type="checkbox" id="women" />
+            <label htmlFor="women">Women</label>
+          </li>
+          <li>
+            <input type="checkbox" id="kids" />
+            <label htmlFor="kids">Baby &amp; Kids</label>
+          </li>
+        </ul>
+      </div>
+      <div classname="filter-section">
+        <h4>OCCASION</h4>
+        <span>All</span>
+      </div>
+      <div classname="filter-section">
+        <h4>WORK</h4>
+        <span>All</span>
+      </div>
+      <div classname="filter-section">
+        <h4>FABRIC</h4>
+        <span>All</span>
+      </div>
+      <div classname="filter-section">
+        <h4>SEGMENT</h4>
+        <span>All</span>
+      </div>
+      <div classname="filter-section">
+        <h4>SUITABLE FOR</h4>
+        <span>All</span>
+      </div>
+      <div classname="filter-section">
+        <h4>RAW MATERIALS</h4>
+        <span>All</span>
+      </div>
+      <div classname="filter-section">
+        <h4>PATTERN</h4>
+        <span>All</span>
+      </div>
     </div>
   );
 };
