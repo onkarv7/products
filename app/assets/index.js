aeb40145fcc89e86ee0b0956605079c907a5f319
onkar vasav <onkarvasav@onkars-MacBook-Air.local>
2025-01-08 21:34:19 +0530

    completed footer  section

--- app/assets/index.js
+++ app/assets/index.js
@@ -2,5 +2,7 @@ import logo from "./Logo.png";
 import lorem from "./lorem.png";
 import search from "./search.png";
 import arrow from "./arrow.png";
+import social from "./social.png";
+import payments from "./payment.png";
 
-export { logo, lorem, search, arrow };
+export { logo, lorem, search, arrow, social, payments };
--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,81 +1,84 @@
-import React from 'react'
-import "./footer.css"
+import React from "react";
+import "./footer.css";
+import { payments, social } from "../assets";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-
-       <footer className="footer">
-  <div className="footer-top">
-    <div className="newsletter">
-      <h3>BE THE FIRST TO KNOW</h3>
-      <p>Sign up for updates from mettà muse.</p> 
-      <div className="newsletter-input">
-        <input type="email" placeholder="Enter your e-mail..." />
-        <button>SUBSCRIBE</button>
+    <footer className="footer">
+      <div className="footer-top">
+        <div className="newsletter">
+          <h3>BE THE FIRST TO KNOW</h3>
+          <p>Sign up for updates from mettà muse.</p>
+          <div className="newsletter-input">
+            <input type="email" placeholder="Enter your e-mail..." />
+            <button className="subscribe-button">SUBSCRIBE</button>
+          </div>
+        </div>
+        <div className="contact-info">
+          <h3>CONTACT US</h3>
+          <p>+44 221 133 5360</p>
+          <p>customercare@mettamuse.com</p>
+          <h3>CURRENCY</h3>
+          <p>
+            <span className="flag">🇺🇸</span> USD
+            <br />
+            <small>
+              Transactions will be completed in Euros and a currency reference
+              is available on hover.
+            </small>
+          </p>
+        </div>
       </div>
-    </div>
-    <div className="contact-info">
-      <h3>CONTACT US</h3>
-      <p>+44 221 133 5360</p>
-      <p>customercare@mettamuse.com</p>
-      <h3>CURRENCY</h3>
-      <p>
-        <span className="flag">🇺🇸</span> USD
-        <br />
-        <small>
-          Transactions will be completed in Euros and a currency reference is
-          available on hover.
-        </small>
-      </p>
-    </div>
-  </div>
-  <hr />
-  <div className="footer-links">
-    <div className="footer-column">
-      <h3>mettā muse</h3>
-      <ul>
-        <li>About Us</li>
-        <li>Stories</li>
-        <li>Artisans</li>
-        <li>Boutiques</li>
-        <li>Contact Us</li>
-        <li>EU Compliances Docs</li>
-      </ul>
-    </div>
-    <div className="footer-column">
-      <h3>QUICK LINKS</h3>
-      <ul>
-        <li>Orders & Shipping</li>
-        <li>Join/Login as a Seller</li>
-        <li>Payment & Pricing</li>
-        <li>Return & Refunds</li>
-        <li>FAQs</li>
-        <li>Privacy Policy</li>
-        <li>Terms & Conditions</li>
-      </ul>
-    </div>
-    <div className="footer-column">
-      <h3>FOLLOW US</h3>
-      <div className="social-icons">
-        <span>🌐</span>
-        <span>🌐</span>
-      </div>
-      <h3>mettā muse ACCEPTS</h3>
-      <div className="payment-icons">
-        <span>GooglePay</span>
-        <span>Visa</span>
-        <span>Amex</span>
-        <span>ApplePay</span>
-      </div>
-    </div>
-  </div>
-  <div className="footer-bottom">
-    <p>Copyright © 2023 mettamuse. All rights reserved.</p>
-  </div>
-</footer>
+      <hr />
+      <div className="footer-links">
+        <div className="footer-column">
+          <h3>mettā muse</h3>
+          <ul>
+            <li>About Us</li>
+            <li>Stories</li>
+            <li>Artisans</li>
+            <li>Boutiques</li>
+            <li>Contact Us</li>
+            <li>EU Compliances Docs</li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h3>QUICK LINKS</h3>
+          <ul>
+            <li>Orders & Shipping</li>
+            <li>Join/Login as a Seller</li>
+            <li>Payment & Pricing</li>
+            <li>Return & Refunds</li>
+            <li>FAQs</li>
+            <li>Privacy Policy</li>
+            <li>Terms & Conditions</li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h3>FOLLOW US</h3>
+          <div className="social-icons">
+            {/* <span>🌐</span>
+            <span>🌐</span> */}
+            <Image src={social} width={100} height={40} />
 
-  
-  )
-}
+          </div>
+          <h3>mettā muse ACCEPTS</h3>
+          <div className="payment-icons">
+            {/* <span>GooglePay</span>
+            <span>Visa</span>
+            <span>Amex</span>
+            <span>ApplePay</span> */}
+                <Image src={payments} width={350} height={30} />
+            
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>Copyright © 2023 mettamuse. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
